36fc98975ff92f0fc672f81118cfffb37c72a19d
manish12312 <82791420+manish12312@users.noreply.github.com>
2021-04-24 16:12:33 +0530

    By git team
    two method added by git team

--- GitDemo6/src/com/cjc/Demo6.java
+++ GitDemo6/src/com/cjc/Demo6.java
@@ -6,7 +6,33 @@ public class Demo6
 	public static void main(String[] args)
 	{
              System.out.println("Hello Demo 6 ");	
+		Demo6 d=new Demo6();
+		d.m1();
+		d.m2();
+		
              
 	}
-
+	
+	public void m1()
+	{
+		 System.out.println("Hello M1");
+	}
+	
+	public void m2()
+	{
+		 System.out.println("Hello M2");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
